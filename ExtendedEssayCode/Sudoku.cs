6f54f7564be068966f5e99f6cd471c31e6bdad8b
NONAME1103 <faparicio1103@gmail.com>
2021-05-25 18:47:28 +0200

    Initial Commit

---
+++ ExtendedEssayCode/Sudoku.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ExtendedEssayCode {
+    
+    public class Sudoku {
+
+        public static int[][] BoardGen(int n) {
+            Console.WriteLine("Sup");
+            int[][] board = new int[n][];
+            return board;
+        }
+    }
+}
