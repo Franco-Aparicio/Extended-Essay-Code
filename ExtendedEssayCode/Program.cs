776f6353125821c6fb41250de443a652e9662bc1
Franco Aparicio <faparicio1103@gmail.com>
2021-06-09 20:01:32 +0200

    Much Interesting

--- ExtendedEssayCode/Program.cs
+++ ExtendedEssayCode/Program.cs
@@ -1,12 +1,19 @@
-﻿using System;
+﻿using System.Threading;
 
 namespace ExtendedEssayCode {
-
     class Program {
+        // characters used in the grid
+        public const string valueList = "123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ?";
 
         static void Main(string[] args) {
-            Sudoku problem = new Sudoku(3);
-            problem.PrintBoard();
+            // start generating grid
+            int n = 4;
+            for (int i = 0; i < 4; i++) {
+                Thread t = new Thread(() => { new Grid(n); });
+                t.Priority = ThreadPriority.Highest;
+                t.Start();
+            }
+            
         }
     }
 }
---
+++ ExtendedEssayCode/State.cs
@@ -0,0 +1,12 @@
+﻿namespace ExtendedEssayCode {
+    struct State {
+        public char Value;
+        public string PossibleValues;
+
+        public State(char value, string possibleValues)
+        {
+            Value = value;
+            PossibleValues = possibleValues;
+        }
+    }
+}
