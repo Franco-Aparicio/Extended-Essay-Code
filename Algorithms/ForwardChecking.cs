46a0f39a87fd272d919c7250ed2bcef1ea037795
Franco Aparicio <faparicio1103@gmail.com>
2021-06-25 15:15:46 +0200

    continue

--- Algorithms/ForwardChecking.cs
+++ Algorithms/ForwardChecking.cs
@@ -7,12 +7,13 @@ namespace Algorithms {
     public class ForwardChecking {
         
         private Dictionary<int[], List<int[]>> allowed;
-        public Dictionary<int[], List<int[]>>.KeyCollection Keys;
+        public List<int[]> Keys;
         public List<int[]> Solution;
         
         public ForwardChecking(Dictionary<int[], List<int[]>> allowed) {
             this.allowed = allowed;
-            Keys = allowed.Keys;
+            Keys = allowed.Keys.ToList();
+            Solution = new List<int[]>();
         }
 
         public bool FC(Variable[] vars) {
@@ -28,33 +29,34 @@ namespace Algorithms {
         private bool Search(Variable[] vars, int level) {
             Random r = new Random();
             Variable var = vars[r.Next(vars.Length)];
+            Console.WriteLine(var.Index);
             for (int i = 0; i < var.Domain.GetLength(1); i++) {
                 if (var.Domain[1, i] != 0) continue;
-                Solution.Add(new int[] {var.Index, var.Domain[0, i]});
+                var item = new int[] {var.Index, var.Domain[0, i]};
+                Solution.Add(item);
                 if (vars.Length == 1) {
                     return true;
                 }
                 if (CheckForward(vars.Where(x => x.Index != var.Index).ToArray(), level, var, var.Domain[0, i]) &&
                     Search(vars.Where(x => x.Index != var.Index).ToArray(), level + 1)) return true;
-                Solution.Remove(new int[] {var.Index, var.Domain[0, i]});
+                Solution.Remove(item);
                 Restore(vars.Where(x => x.Index != var.Index).ToArray(), level);
             }
             return false;
         }
         
         private bool CheckForward(Variable[] vars, int level, Variable var, int val) {
-            foreach (Variable variable in vars) {
-                int[] k = var.Index < variable.Index ? new [] {var.Index, variable.Index} : new [] {variable.Index, var.Index};
-                var key = Keys.ToList().Find(x => x.SequenceEqual(k));
-                if (key != null) {
-                    for (int i = 0; i < variable.Domain.GetLength(1); i++) {
-                        if (variable.Domain[1, i] == 0 &&
-                            allowed[key].Find(x=>x.SequenceEqual(new int[] {val, variable.Domain[0, i]})) == null) {
-                            variable.Domain[1, i] = level;
+            foreach (Variable v in vars) {
+                int[] k = var.Index < v.Index ? new [] {var.Index, v.Index} : new [] {v.Index, var.Index};
+                var key = Keys.Find(x => x.SequenceEqual(k));
+                if (key == null) continue;
+                    for (int i = 0; i < v.Domain.GetLength(1); i++) {
+                        if (v.Domain[1, i] == 0 &&
+                            !allowed[key].Any(x=>x.SequenceEqual(new int[] {val, v.Domain[0, i]}))) {
+                            v.Domain[1, i] = level;
                         }
                     }
-                    if (DWO(variable)) return false;
-                }
+                    if (DWO(v)) return false;
             }
             return true;
         }
